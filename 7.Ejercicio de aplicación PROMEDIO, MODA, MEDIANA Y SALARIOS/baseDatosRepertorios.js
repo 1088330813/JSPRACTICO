6b896abe1b64e82b9dd317079959d8abce5f3ed2
Jorge Sánchez <bugsmario9510@gmail.com>
2021-10-10 23:19:38 -0500

    Adelanto de ingreso de datos a array desde html

---
+++ 7.Ejercicio de aplicación PROMEDIO, MODA, MEDIANA Y SALARIOS/baseDatosRepertorios.js
@@ -0,0 +1,16 @@
+const repertorios = [];
+repertorios.push({
+    nombre: "Cristo No está Muerto",
+    tonalidad: "F",
+    tema: "Poder",
+});
+repertorios.push({
+    nombre: "Alabaré",
+    tonalidad: "G",
+    tema: "Alabanza",
+});
+repertorios.push({
+    nombre: "Alabad a Jehova",
+    tonalidad: "F",
+    tema: "Alabanza",
+});
--- 7.Ejercicio de aplicación PROMEDIO, MODA, MEDIANA Y SALARIOS/repertorios.js
+++ 7.Ejercicio de aplicación PROMEDIO, MODA, MEDIANA Y SALARIOS/repertorios.js
@@ -1,32 +1,44 @@
-const repertorios = [];
-repertorios.push({
-    nombre: "Cristo No está Muerto",
-    tonalidad: "F",
-    tema: "Poder",
-});
-repertorios.push({
-    nombre: "Alabaré",
-    tonalidad: "G",
-    tema: "Alabanza",
-});
-repertorios.push({
-    nombre: "Alabad a Jehova",
-    tonalidad: "F",
-    tema: "Alabanza",
-});
-
 const nombreCanciones = repertorios.map(
     function(canciones) {
         return canciones.nombre;
 
     });
 const nombreCancionesOrdenada = repertorios.sort(
-    function(nombreA, nombreB) {
-        return (nombreA - nombreB)
+    function(a, b) {
+        if (a.nombre > b.nombre) {
+            return 1;
+        }
+        if (a.nombre < b.nombre) {
+            return -1;
+        } else { 0 };
+    });
+
+const nombreCancionesPorTonalidadFa = nombreCancionesOrdenada.filter(
+    function(canciones) {
+        return canciones.tonalidad == "F";
     });
 
 const nombreCancionesPorTonalidad = repertorios.filter(
     function(canciones) {
         return canciones.tonalidad == "F";
-    }
-)
+    });
+
+const nombreCancionesPrimeraCancion = repertorios.find(
+    function(canciones) {
+        return canciones.tonalidad == "F";
+    });
+
+function guardarInfo() {
+    const inputA = document.getElementById("NombreCancion");
+    const valueA = inputA.value;
+    const inputB = document.getElementById("Tonalidad");
+    const valueB = inputB.value;
+    const inputC = document.getElementById("Tematica");
+    const valueC = inputC.value;
+
+}
+repertorios.push({
+    nombre = valueA,
+    tonalidad = valueB,
+    tema = valueC,
+});
